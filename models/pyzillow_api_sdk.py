ec9f64ab703fc3ad2e2fd5852bc441b66cdb33a5
OzzyCodes <ajwaldro@gmail.com>
2021-04-05 00:05:45 -0400

    test web scraping scripts added for zillow and using trulia

--- models/pyzillow_api_sdk.py
+++ models/pyzillow_api_sdk.py
@@ -1,15 +1,12 @@
 import requests
+from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults
 
 url = "https://www.zillow.com/homes/Plymouth-County,-MA_rb/"
 
-payload = "zpid=%3CREQUIRED%3E&chartDuration=%5B%221year%22%2C%225years%22%2C%2210years%22%5D&unit-type=dollar&zws-id" \
-          "=%3CREQUIRED%3E "
-headers = {
-    'content-type': "application/x-www-form-urlencoded",
-    'x-rapidapi-key': "0b6e659976msh33d9dcf51fc07dcp161394jsn637923d981f1",
-    'x-rapidapi-host': "ZillowdimashirokovV1.p.rapidapi.com"
-}
+zillow_data = ZillowWrapper('X1-ZWz16pqqcqqhhn_11xce')
+deep_search = zillow_data.get_data("https://www.zillow.com/homes/Plymouth-County,-MA_rb/", {
+    'zipcode': '02339'
+})
 
-response = requests.request("POST", url, data=payload, headers=headers)
 
-print(response.text)
+
